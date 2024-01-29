1941344ae6165e8995cca6fcf14b8194a0e1b47e
Antoine Belvire <antoine.belvire@opensuse.org>
2024-01-29 19:57:07 +0100

    Use words from bundled UKACD as fallback

--- croissant-cli/src/main.rs
+++ croissant-cli/src/main.rs
@@ -4,17 +4,18 @@ use croissant_solver_cadical::CadicalSolver;
 use croissant_solver_logicng::LogicngSolverBuilder;
 use croissant_solver_splr::SplrSolverBuilder;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 use std::path::PathBuf;
 
+/// 🥐 Welcome to Croissant, a crossword solver that smells good.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// The grid.
+    /// The grid as a string; Each new line is a new row, '.' is a blank, '#' is a block.
     grid: String,
     /// The path to the word list.
     #[arg(short, long)]
-    word_list: String,
+    wordlist: Option<PathBuf>,
     /// The solver to use.
     #[arg(short, long, default_value = "logicng")]
     solver: String,
@@ -25,16 +26,28 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let words = read_words(args.word_list);
+    let words = args.wordlist.map(|path| read_words_at(path)).unwrap_or_else(|| ukacd());
     let crossword = Crossword::try_from(args.grid.as_str(), &words).unwrap();
     let mut solutions = solve(crossword, args.solver);
     iterate_and_print(args.count, &mut solutions);
 }
 
-fn read_words(path: String) -> Vec<String> {
-    let path = PathBuf::from(path);
+/// Reads words from the file at given path. Panics if no such file exists.
+fn read_words_at(path: PathBuf) -> Vec<String> {
     let file = File::open(path).expect("Test word list not found");
-    BufReader::new(file)
+    read(file)
+}
+
+/// Reads words from bundled UKACD.
+fn ukacd() -> Vec<String> {
+    // FIXME it's quite brittle to reference file in a test directory of another project, find a way to share resource
+    let bytes_of_ukacd = include_bytes!("../../croissant-solver-logicng/tests/UKACD18plus.txt");
+    read(&bytes_of_ukacd[..])
+}
+
+/// Reads and sanitizes words from a source supporting [Read].
+fn read<T: Read>(data: T) -> Vec<String> {
+    BufReader::new(data)
         .lines()
         .map(Result::unwrap)
         .map(|word| word.replace(['-', '\'', '.'], "").to_uppercase())
@@ -43,6 +56,7 @@ fn read_words(path: String) -> Vec<String> {
         .collect()
 }
 
+/// Solves (lazily) the grid with the solver
 fn solve(crossword: Crossword, solver_name: String) -> CrosswordSolutions {
     match solver_name.as_str() {
         // TODO create an enum
@@ -69,6 +83,7 @@ fn iterate_and_print(count: usize, solutions: &mut CrosswordSolutions) {
                 } else {
                     println!("No more solution.")
                 }
+                break;
             }
             Some(grid) => {
                 if number > 1 {
